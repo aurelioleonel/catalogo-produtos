8c29536e8bd66989e7aac587cc3509afcbe3a7bc
MarcosAurelio <aurelioleonel@gmail.com>
2023-04-06 22:56:16 -0300

    Informações adicionionais do Token

--- src/main/java/com/devsuperior/dscatalog/config/AppConfig.java
+++ src/main/java/com/devsuperior/dscatalog/config/AppConfig.java
@@ -1,12 +1,17 @@
 package com.devsuperior.dscatalog.config;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 public class AppConfig {
+    @Value("${jwt.secret}")
+    private String jwtSrecret;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -14,4 +19,17 @@ public class AppConfig {
         return new BCryptPasswordEncoder();
 
     }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+        tokenConverter.setSigningKey(jwtSrecret);
+        return tokenConverter;
+    }
+
+    @Bean
+    public JwtTokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
 }
